145591ff10d6f9ac69622bd8e6d52f931a89151b
Mundolel <samuxdrbx@gmail.com>
2025-06-03 20:03:18 -0500

    init commit

--- src/main/java/ads/tiendopolis/controllers/DevolucionController.java
+++ src/main/java/ads/tiendopolis/controllers/DevolucionController.java
@@ -1,4 +1,13 @@
 package ads.tiendopolis.controllers;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class DevolucionController extends BaseController{
+
+    @FXML
+    protected void gestionView() throws IOException {
+        showScene("gestion-view");
+    }
 }
--- src/main/java/ads/tiendopolis/controllers/NotificacionController.java
+++ src/main/java/ads/tiendopolis/controllers/NotificacionController.java
@@ -1,5 +1,13 @@
 package ads.tiendopolis.controllers;
 
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class NotificacionController extends BaseController{
 
+    @FXML
+    protected void gestionView() throws IOException {
+        showScene("gestion-view");
+    }
 }
